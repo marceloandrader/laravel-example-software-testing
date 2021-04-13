ee937a15951e2e050eef9e55ef557d6c364c9fd3
Marcelo Andrade R <marcelo.andrade.r@gmail.com>
2021-04-13 07:14:31 -0500

    crear modelo y migracion

---
+++ app/Models/Estudiante.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Estudiante extends Model
+{
+    use HasFactory;
+
+    protected $casts = [
+        'fecha_de_nacimiento' => 'date:Y-m-d',
+    ];
+}
