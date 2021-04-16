88850fd140f1cfd6f9f631868642c76c9f22c3ea
Marcelo Andrade R <marcelo.andrade.r@gmail.com>
2021-04-16 07:07:03 -0500

    test de integracion de crear estudiante

---
+++ app/Http/Controllers/EstudianteController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Estudiante;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EstudianteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $estudiante = new Estudiante();
+        $estudiante->apellidos = $data['apellidos'];
+        $estudiante->nombres = $data['nombres'];
+        $estudiante->fecha_de_nacimiento = $data['fecha_de_nacimiento'];
+        $estudiante->ciudad_de_nacimiento = $data['ciudad_de_nacimiento'];
+
+        $estudiante->save();
+
+        return response()->noContent(Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Estudiante  $estudiante
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Estudiante $estudiante)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Estudiante  $estudiante
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Estudiante $estudiante)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Estudiante  $estudiante
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Estudiante $estudiante)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Estudiante  $estudiante
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Estudiante $estudiante)
+    {
+        //
+    }
+}
---
+++ tests/Feature/CrearEstudianteTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CrearEstudianteTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_crear_un_estudiante_con_todos_los_datos()
+    {
+        $response = $this->post('/estudiantes', [
+            'apellidos' => 'Andrade',
+            'nombres' => 'Marcelo',
+            'fecha_de_nacimiento' => date_create('15 years ago')->format('Y-m-d'),
+            'ciudad_de_nacimiento' => 'Tulcán',
+        ]);
+
+        $response->assertStatus(201);
+    }
+}
